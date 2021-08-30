b377a70e53de2f7076b6c6da7eb467434c6c1280
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-30 14:06:49 -0300

    Update Exer1.py

--- Exer1.py
+++ Exer1.py
@@ -1,8 +1,17 @@
+#Declaração de variáveis input para inserção de dados
+#input da altura da parede
 a = float(input("Insira a altura da parede: "))
+#input do comprimento da parede
 c = float(input("Insira o comprimento da parede: "))
+#calculo da área da parede
 area = (a*c)
+#cálculo da quantia de litros de tinta para pintar a parede
 litros = (area/3)
+#cálculo da quantia de latas de tinta para pintar a parede
 latas = (litros/3.6)
+#cálculo do valor das latas de tinta necessárias
 valor = (latas*107)
+#Resultado da quatia de latas de tinta
 print("Precisará de {:.2f} latas de tinta".format(litros)+
+#Resultado do valor das latas de tinta
 "\nCustará R${:.2f}".format(valor))
