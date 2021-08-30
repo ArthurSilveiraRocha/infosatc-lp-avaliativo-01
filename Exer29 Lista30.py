68136e0b6a265ed3f1bf82f81af80ac5fdd95ca1
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-30 14:28:19 -0300

    Create Exer29 Lista30.py

---
+++ Exer29 Lista30.py
@@ -0,0 +1,9 @@
+#input de quatro valores float
+w = float(input("Insira nota 1: "))
+x = float(input("Insira nota 2: "))
+y = float(input("Insira nota 3: "))
+z = float(input("Insira nota 4: "))
+#Cálculo da media dos quatro inputs
+m = (w+x+y+z)/4
+#Resultado da média
+print(m)
