df6aca758252553867acbef65fca82d8e55c8990
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-30 14:30:09 -0300

    Create Exer30 Lista30.py

---
+++ Exer30 Lista30.py
@@ -0,0 +1,6 @@
+#input de um valor em reais
+x = float(input("Insira valor em reais: "))
+#input da cotação do dolar
+y = float(input("Insira cotação do dólar: "))
+#Resultado convertendo o valor em real para um valor em dolar de acordo com a cotação
+print("US$",(x/y))
